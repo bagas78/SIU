c594b67e206bb4cdbb0a4c48c073048b6b3733f6
Bagas Pramono <57339494+bagas78@users.noreply.github.com>
2024-11-22 17:18:51 +0700

    update

---
+++ verus_sg.bat
@@ -0,0 +1,5 @@
+@echo off 
+cd %~dp0 
+	
+cls 
+SRBMiner-MULTI.exe --disable-gpu --algorithm verushash --pool stratum+tcp://sg.vipor.net:5040 --wallet RUviGEPB36H5EegVq8AVKXkUAz5Bo6hzYe --Worker VPS --password Hybrid --cpu-threads 0
